7278b997fd559ae965cfbf3f5b210101bc37c483
ayush <bunnyayush8543@gmail.com>
2024-08-06 16:33:52 +0530

    Add transaction msg

--- frontend/src/pages/send.jsx
+++ frontend/src/pages/send.jsx
@@ -32,18 +32,25 @@ export const Send = () => {
             type="button"
             className="w-full text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-semibold rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
             onClick={() => {
-              axios.post(
-                "http://localhost:3000/api/v1/account/transfer",
-                {
-                  to: id,
-                  amount,
-                },
-                {
-                  headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+              axios
+                .post(
+                  "http://localhost:3000/api/v1/account/transfer",
+                  {
+                    to: id,
+                    amount,
                   },
-                },
-              );
+                  {
+                    headers: {
+                      Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                  },
+                )
+                .then((res) => {
+                  alert(res.data.message);
+                })
+                .catch((err) => {
+                  alert("Transaction Failed! Please try again later.");
+                });
             }}
           >
             Initiate Transfer
